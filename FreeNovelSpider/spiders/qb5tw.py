aad55e42a29915a566d27d9d5a9064a867d54dee
hmx123 <17839192461@163.com>
2019-10-30 10:44:42 +0800

    新增全本小说网采集

--- FreeNovelSpider/spiders/qb5tw.py
+++ FreeNovelSpider/spiders/qb5tw.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
-import random
+
 import re
 from datetime import datetime
-import json
+
 import time
 import pymysql
 import scrapy
 from scrapy.utils.project import get_project_settings
 from elasticsearch import Elasticsearch
 from scrapy_redis.spiders import RedisSpider
-from lxml import etree
-# http://www.xbiquge.la/
+
 
 def setargs(author, target, label, conn):
     cursor = conn.cursor()
@@ -36,11 +35,11 @@ def setargs(author, target, label, conn):
 
 
 class XBiquSpider(RedisSpider):
-    name = 'xbiqu'
-    allowed_domains = ['www.xbiquge.la']
+    name = 'qb5tw'
+    allowed_domains = ['www.qb5.tw']
     # start_urls = ['http://www.biquga.com/']
     # http://www.xbiquge.la/42/42515/  小说详情页
-    redis_key = 'xbiquspider:start_urls'
+    redis_key = 'qb5tw:start_urls'
     # 将配置文件读到内存中，是一个字典
     settings = get_project_settings()
     host = settings['DB_HOST']
@@ -55,34 +54,39 @@ class XBiquSpider(RedisSpider):
     es = Elasticsearch()
 
     def parse(self, response):
-        # http://www.xbiquge.la/42/42515/
+        # https://www.qb5.tw/shu/112022.html
         parse_url = response.url
         # 解析当前url
-        bookId = parse_url.split('/')[-2]
+        bookId = parse_url.split('/')[-1].split('.')[0]
         conn = pymysql.Connect(host=self.host, port=self.port, user=self.user, password=self.password, db=self.dbname, charset=self.dbcharset)
         self.conn = conn
         cursor = conn.cursor()
         # 解析小说
         novel_name = response.xpath('//div[@id="info"]/h1/text()').extract_first()
-        novel_author = response.xpath('//div[@id="info"]/p[1]/text()').extract_first().split('：')[1]
-        novel_img = response.xpath('//div[@id="fmimg"]/img/@src').extract_first()
-        novel_info = response.xpath('//div[@id="intro"]/p[2]/text()').extract_first()
-        novel_lastupt = response.xpath('//div[@id="info"]/p[3]/text()').extract_first().split('：')[1]
-        novel_type = response.xpath('//div[@class="con_top"]/a[2]/text()').extract_first()
-        chapters = response.xpath('//div[@id="list"]/dl/dd/a/@href')
-        type_dict = {'修真小说': '5', '玄幻小说': '4', '都市小说': '3', '历史小说': '6', '网游小说': '14', '科幻小说': '8', '其他小说': '4'}
+        novel_author = response.xpath('//div[@class="options"]/a[1]/text()').extract_first().split('：')[1]
+        novel_img = response.xpath('//div[@class="img_in"]/img/@src').extract_first()
+        novel_infos = response.xpath('//div[@id="intro"]/text()')
+        novel_info = '\t\r'.join(x.extract().strip() for x in novel_infos)
+        novel_lastupt = response.xpath('//div[@class="update"]/text()').getall()[1].strip().strip('（）')
+        novel_type = response.xpath('//div[@class="nav-mbx"]/a[2]/text()').extract_first()
+        chapters = response.xpath('//dl[@class="zjlist"]/dd/a/@href')
+        word_count = response.xpath('//div[@class="options"]/span[1]/text()').extract_first()
+        # 正则匹配到数字
+        pattern = re.compile(r'\d+')   # 查找数字
+        novel_word_count = '%s0000' % pattern.findall(word_count)[0]
+        type_dict = {'仙侠修真': '5', '玄幻魔法': '4', '都市言情': '3', '历史军事': '6', '网游竞技': '14', '科幻灵异': '8', '恐怖惊悚': '7', '其他类型': '4'}
         try:
             label = type_dict[novel_type]
         except:
             label = '4'
-        updated = time.mktime(time.strptime(novel_lastupt, "%Y-%m-%d %H:%M:%S"))
+        updated = time.mktime(time.strptime(novel_lastupt, "%Y-%m-%d %H:%M"))
         # 判断小说是否存在数据库
-        sql_find = "select id from novels where name='%s' and bookId='%s' and novel_web=2;"
+        sql_find = "select id from novels where name='%s' and bookId='%s' and novel_web=3;"
         cursor.execute(sql_find % (novel_name, bookId))
         fin = cursor.fetchone()
         if not fin:
             # 采集
-            words = 0
+            words = novel_word_count
             now_time = datetime.now()
             now_time = now_time.strftime("%Y-%m-%d %H:%M:%S")
             setargs(novel_author, [], '都市', conn)    # 将作者添加作者表 words created
@@ -95,7 +99,7 @@ class XBiquSpider(RedisSpider):
                 print(e)
             sql = "insert into novels(name,cover,summary,label,state,words,created,updated,authorId,target,score,bookId,addtime,novel_web,updatetime) values('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s');"
             cursor.execute(sql % (
-                novel_name, novel_img, novel_info, label, '1', words, '0', updated, authorId, '', '9.5',  bookId, now_time, '2', now_time
+                novel_name, novel_img, novel_info, label, '1', words, '0', updated, authorId, '', '9.5',  bookId, now_time, '3', now_time
             ))
             conn.commit()
             # 获取小说id
@@ -108,7 +112,7 @@ class XBiquSpider(RedisSpider):
             for chapter in chapters:
                 chapter_count += 1
                 # 拼接章节url http://www.xbiquge.la/19/19523/10080224.html
-                url = 'http://www.xbiquge.la' + chapter.extract()
+                url = 'https://www.qb5.tw' + chapter.extract()
                 yield scrapy.Request(url=url, callback=self.parse_chaper, meta={'novelId': novelId, 'bookId': bookId, 'chapterId': chapter_count})
         else:
             #更新
@@ -121,15 +125,15 @@ class XBiquSpider(RedisSpider):
                 # 判断章节是否存在
                 sql_find = "select id from chapters where novelId='%s' and chapterId='%s';"
                 chapter_url = chapter.extract()
-                chapterId  = chapter_url.split('/')[-1].split('.')[0]
+                chapterId  = chapter_count
                 # 判断数据库是否存在 novelId chapterId
                 if not cursor.execute(sql_find % (novelId, chapterId)):
                     # 更新小说表小说更新时间
                     sql_update = "update novels set updated='%s',updatetime='%s' where id='%s';"
                     cursor.execute(sql_update % (updated, now_time, novelId))
                     conn.commit()
-                    # 拼接章节url http://www.xbiquge.la/19/19523/10080224.html
-                    url = 'http://www.xbiquge.la' + chapter_url
+                    # 拼接章节url https://www.qb5.tw/shu/40714/13641063.html
+                    url = 'https://www.qb5.tw' + chapter_url
                     yield scrapy.Request(url=url, callback=self.parse_chaper, meta={'novelId': novelId, 'bookId': bookId, 'chapterId': chapter_count})
         sql = "update novels set chaptercount= '%s' where id='%s';"
         cursor.execute(sql % (chapter_count, novelId))
@@ -137,20 +141,16 @@ class XBiquSpider(RedisSpider):
 
     def parse_chaper(self, response):
         '''请求章节数据'''
-        # http://www.xbiquge.la/19/19523/10080224.html
+        # https://www.qb5.tw/shu/40714/13834628.html
         cursor = self.conn.cursor()
         chapterId = response.meta['chapterId']
         novelId = response.meta['novelId']
         bookId = response.meta['bookId']
         # 获取章节名字
-        chapter_name = response.xpath('//div[@class="bookname"]/h1/text()').extract_first()
-        content = response.xpath('//div[@id="content"]').extract_first()
+        chapter_name = response.xpath('//div[@id="main"]/h1/text()').extract_first()
+        contents = response.xpath('//div[@id="content"]/text()')[1:-1]
+        content = '\t\r'.join(x.extract().strip() for x in contents).replace('\t\r\t\r', '\t\r')
 
-        # 去掉div  去掉空格  正则匹配p标签 去掉
-        content = content.replace('<div id="content">', '').replace('</div>', '')
-        content = content.replace(' ', '').replace('<br>', '').replace('\r\r', '\t\r')
-        reg = '<p>(.*)</p>'
-        content = re.sub(reg, '', content)
         words = len(content)
         # 将内容插入章节
         sql = "insert into chapters(name,created,updated,words,novelId,chapterId) values('%s','%s','%s','%s','%s','%s');"
